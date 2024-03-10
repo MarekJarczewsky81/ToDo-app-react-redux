c2de1523428f7b9cb40b2f103d82d8c493317fb0
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-03-10 20:32:27 +0100

    Added Reducer to Store

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,14 +1,16 @@
 import styles from "./ColumnForm.module.scss";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import Button from "../Button/Button.js";
 
 const ColumnForm = props => {
     const [title, setTitle] = useState("");
     const [icon, setIcon] = useState("");
+    const dispatch = useDispatch();
     
     const handleSubmit = e => {
       e.preventDefault();
-      props.action({ title: title, icon: icon });
+      dispatch({ type: "ADD_COLUMN", payload: { title, icon } });
       setTitle("");
       setIcon("");
     };
